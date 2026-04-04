a2ba3daaf16bd9c6bde28c3d50050a4e36f9bf6e
刘金发 <liujinfa9242@dingtalk.com>
2026-04-05 01:02:57 +0800

    fix(settings): update GitHub repository link to point to the correct owner

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -20,7 +20,7 @@ const SettingPage = () => {
   }
 
   const toGithubRepo = useLockFn(() => {
-    return openWebUrl('https://github.com/clash-verge-rev/clash-verge-rev')
+    return openWebUrl('https://github.com/iuin8/clash-verge-rev')
   })
 
   const toGithubDoc = useLockFn(() => {
